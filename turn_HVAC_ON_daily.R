b22e3cd6fdcb298eb0b259c3db56903c440acbe0
Jas Sohi <j450h1@gmail.com>
2018-08-05 23:58:45 -0700

    Send telegram notification as well now

--- turn_HVAC_ON_daily.R
+++ turn_HVAC_ON_daily.R
@@ -4,6 +4,7 @@ source(environment_variable_file)
 source('functions.R')
 
 MINIMUM_TEMPERATURE <- 24 #Celsius
+TELEGRAM_MESSAGE <- "The HVAC has been turned on!"
 
 # Authenticate
 token <- get_access_token()
@@ -12,6 +13,17 @@ vehicle_id_s <- get_vehicle_id_s(token)
 
 inside_temp <- get_internal_temperature(token, vehicle_id_s)
 
+send_telegram_message <- function(text, chat_id, bot_token){ 
+  require(telegram) 
+  bot <- TGBot$new(token = bot_token) 
+  bot$sendMessage(text = text, chat_id = chat_id)
+}
+
 if (inside_temp > MINIMUM_TEMPERATURE) {
   start_hvac(token, vehicle_id_s)
+  # Remember to set environment variables for telegram as well
+  send_telegram_message(TELEGRAM_MESSAGE, 
+                        Sys.getenv("TELEGRAM_CHAT_ID"), 
+                        Sys.getenv("TELEGRAM_BOT_TOKEN"))
 }
+
