8ac09cf153cd10f6201e1c7e3ef711bc69ca66df
Jas Sohi <j450h1@gmail.com>
2020-02-01 22:15:32 -0800

    Closer to running on cloud

--- googleCloudRunner_test.R
+++ googleCloudRunner_test.R
@@ -6,12 +6,8 @@ r_lines <- c("list.files()",
              "mtcars %>% select(mpg)",
              "sessionInfo()")
 
-# example code runs against a source that is a mirrored GitHub repo
-source <- cr_build_source(RepoSource("googleCloudStorageR",
-                                     branchName = "master"))
-
 # check the script runs ok
-cr_deploy_r(r_lines, source = source)
+cr_deploy_r(r_lines)
 
-# schedule the script once its working
-cr_deploy_r(r_lines, schedule = "15 21 * * *", source = source)
+# schedule the script once its working  
+cr_deploy_r(r_lines, schedule = "15 21 * * *")
