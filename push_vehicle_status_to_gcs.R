cbb50d87708ca01012e5affdfd560e0b2f7cfb3c
Jas Sohi <j450h1@gmail.com>
2021-02-16 01:11:04 -0800

    Temporary workaround

--- push_vehicle_status_to_gcs.R
+++ push_vehicle_status_to_gcs.R
@@ -4,23 +4,29 @@ source(here::here('functions.R'))
 library(tidyverse)
 library(googleCloudStorageR)
 library(bigrquery)
+library(httr)
 
 # Authenticate
 gcs_auth(Sys.getenv("GCP_KEYPATH"))
 bq_auth(email = Sys.getenv("BQ_EMAIL"))
 
 # Authenticate with Tesla
-token <- get_access_token()
+#token <- get_access_token()
 
 # Get vehicle id
-vehicle_id_s <- get_vehicle_id_s(token)
+#vehicle_id_s <- get_vehicle_id_s(token)
 
 # Wake vehicle up
-wake_vehicle_up(token, vehicle_id_s)
+#wake_vehicle_up(token, vehicle_id_s)
 
 # Get data
-Sys.sleep(60)
-tesla_data <- get_vehicle_data(token, vehicle_id_s)
+#Sys.sleep(60)
+#tesla_data <- get_vehicle_data(token, vehicle_id_s)
+URL <- "https://python-app-j4f32r553a-uw.a.run.app"
+
+tesla_data <- URL %>%
+  httr::GET() %>%
+  content()
 
 # Push vehicle state data to GCS
 today <- as.character(Sys.Date(), format = "%Y%m%d")
